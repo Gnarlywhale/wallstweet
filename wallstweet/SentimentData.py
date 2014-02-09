994d5d80245d736cf4364376be0ab6d4c4c8a946
Kent Rasmussen <hyprkookeez@gmail.com>
2014-02-09 03:01:25 -0700

    Testing

--- wallstweet/SentimentData.py
+++ wallstweet/SentimentData.py
@@ -4,30 +4,29 @@ class SentimentData:
 	def __init__(self):
 		self.db = MySQLdb.connect(host="localhost", user="wallstweet", db="wallstweet")
 		cur = self.db.cursor()
-		cur.execute("SELECT text, rating FROM sentiment_dataset")
+		cur.execute("SELECT text, rating FROM sentiment_dataset LIMIT 500")
 		
-		self.words = []
-		self.data = []
+		words = []
+		data = []
 		for (text, rating) in cur.fetchall():
 			# Add to the list of data
 			words_filtered = [e.lower() for e in text.split() if len(e) >= 3]
-			self.data.append((words_filtered, rating))
+			data.append((words_filtered, rating))
 		
 			# Add all words in the data to all of the words
-			for word in words_filtered:
-				self.words.append(word)
+			words.extend(words_filtered)
 		cur.close()
 		
-		self.word_features = nltk.FreqDist(self.words).keys()
-		self.training_set = nltk.classify.apply_features(self._extract_features, self.data)
-		self.classifier = nltk.NaiveBayesClassifier.train(self.training_set)
+		self.word_features = nltk.FreqDist(words).keys()
+		training_set = nltk.classify.apply_features(_sentiment_features, data)
+		self.classifier = nltk.NaiveBayesClassifier.train(training_set)
 		
-	def _extract_features(self, string):
-		string_words = set(string)
+	def _sentiment_features(string):
+		words = set(string.split())
 		features = {}
 		for word in self.word_features:
 			features['contains(%s)' % word] = (word in string_words)
 		return features
 		
 	def classify_data(self, string):
-		return self.classifier.classify(_extract_features(string))
+		return self.classifier.classify(self._sentiment_features(string))
